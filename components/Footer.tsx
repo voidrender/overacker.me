d10771aad84f6f903d67d62d20b34e6b10b16555
Isaac Overacker <isaac@alchemyrpg.com>
2022-08-29 01:31:04 -0700

    💄 Change footer copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,9 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>
+        © 2013–{new Date().getFullYear()} {config.author}
+      </div>
 
       <div className={styles.settings}>
         {hasMounted && (
