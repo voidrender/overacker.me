903ef4f7ca1fe968e136a0e69e8f9e7f8ea1c969
Isaac Overacker <isaac@alchemyrpg.com>
2022-08-29 01:27:07 -0700

    💄 Hide table of contents

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -194,7 +194,8 @@ export const NotionPage: React.FC<types.PageProps> = ({
   const isBlogPost =
     block?.type === 'page' && block?.parent_table === 'collection'
 
-  const showTableOfContents = !!isBlogPost
+  // const showTableOfContents = !!isBlogPost
+  const showTableOfContents = false;
   const minTableOfContentsItems = 3
 
   const pageAside = React.useMemo(
